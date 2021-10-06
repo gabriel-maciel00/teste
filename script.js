7401ef35b24bf646ef71e446b4f94587d9af8ff2
Gabriel Maciel <gabriel.santosmaciel.53@gmail.com>
2021-10-06 17:34:32 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+
+   function mostrarLegenda()
+   {
+      var git = document.getElementById("git");
+     var git-fomdocument.getElementById("git-form").style.display = 'block';
+      
+      if (git.style.display == 'none') {
+        git.style.display = 'block';
+    } else {
+        git.style.display = 'none';
+    }
+   }
